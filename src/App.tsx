e4cec9ea5eea6e11cec854a842b3141cbdc02855
jonathan.lin1125@gmail.com <jonathan.lin1125@gmail.com>
2023-07-16 00:10:10 -0700

    cat profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Layout } from 'antd';
-import { Content, Footer } from 'antd/es/layout/layout';
 
 function App() {
+
+  const getContent = () => {
+    return "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Layout>
-          <Content>HOLDER</Content>
-          <Footer>{"FOOTER"}</Footer>
-        </Layout>
-      </header>
+      <div className='headshot-picture'>
+        <img src={require('./cat.jpg')} />
+      </div>
+      <div className='description'>
+        {getContent()}
+      </div>
     </div>
   );
 }
